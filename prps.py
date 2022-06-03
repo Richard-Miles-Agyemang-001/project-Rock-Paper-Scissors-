87f0db60331a5966b7681149abdb50dfce4fe98d
richard-miles-agyemang-001 <richardayemang601@gmail.com>
2022-06-03 11:55:59 +0000

    made some renaming

--- prps.py
+++ prps.py
@@ -1,25 +1,26 @@
+# project-Rock-Paper-Scissors-
 import random
 
 def play ():
-    user = input("What is your choice? 'R for Rock, 'P for Paper, 'S for Scissors\n")
-    user + user.upper()
+    Player = input("What is your choice? 'R for Rock, 'P for Paper, 'S for Scissors\n")
+    Player + Player.upper()
 
-    computer = random.choice(['R', 'P', 'S'])
+    CPU = random.choice(['R', 'P', 'S'])
     
-    if user == computer:
-        return "You and the computer have both chosen {}, It's a tie.",format(computer)
+    if Player == CPU:
+        return "You and the CPU have both chosen {}, It's a tie.",format(CPU)
 
         # R=R, S=S, P=P
-        if is_win(user, computer):
-            return "you have chosen {} and the computer has chosen{}. You WON!", format(user, computer)
+        if is_win(Player, CPU):
+            return "you have chosen {} and the CPU has chosen{}. You WON!", format(Player, CPU)
 
-            return "You have {} and the computer has chosen. You LOSE!", format(user, computer)
+            return "You have {} and the CPU has chosen. You LOSE!", format(Player, CPU)
 
-            def is_win(player, opponent):
-                #return true is the player beats the opponent
+            def is_win(player, CPU):
+                #return true is the player beats the CPU
                 #winning condition R>S, S>P, P>R
-                
-                if (player == 'R' and opponent == 'S') or (player == 'S' and opponent =='P') or (player =='P' and opponent =='R'):
+                if(player == 'R' and CPU == 'S') or (player == 'S' and CPU =='P') or (player =='P' and CPU =='R'):
+
                 return True
                 return False
 
